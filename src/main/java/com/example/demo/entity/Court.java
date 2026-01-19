c4d721bcb0d36255a95be2eb93e36225bfb14656
santogarrido <santocildesgarridoraul@gmail.com>
2026-01-19 13:26:15 +0100

    Creations of the models

---
+++ src/main/java/com/example/demo/entity/Court.java
@@ -0,0 +1,40 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Court {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String category;
+	
+	@ManyToOne
+	@JoinColumn(name = "facility_id")
+	private Facility facility;
+	
+	@OneToMany(mappedBy = "reserve", cascade = CascadeType.ALL, orphanRemoval = false)
+	private List<Reserve> reserves;
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	private String secondName;
+	private String email;
+	private String username;
+	private String img;
+	private String password;
+	private String role;
+	
+	@OneToMany(mappedBy = "reserve", cascade = CascadeType.ALL, orphanRemoval = false)
+	private List<Reserve> reserves;
+	
+}
