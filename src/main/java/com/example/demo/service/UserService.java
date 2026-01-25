ab832c019394713e8023ed7d53f8660ac049e6a7
Flagn3 <flagn3@gmail.com>
2026-01-25 15:06:09 +0100

    create UserService

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,22 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.entity.User;
+import com.example.demo.model.UserDTO;
+
 public interface UserService {
+
+	List<UserDTO> listAllUsers();
+
+	UserDTO getUserById(int id);
+
+	int updateUser(UserDTO userDTO);
+
+	int deleteUser(int id);
+
+	int activateUser(int id);
+
+	int deactivateUser(int id);
 
 }
