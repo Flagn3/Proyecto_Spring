d5872fb9c1f711825d597bd3c4671465523c3717
Flagn3 <flagn3@gmail.com>
2026-02-04 18:40:58 +0100

    Edit table names

--- src/main/java/com/example/demo/entity/Court.java
+++ src/main/java/com/example/demo/entity/Court.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "courts")
 public class Court {
 
 	@Id
